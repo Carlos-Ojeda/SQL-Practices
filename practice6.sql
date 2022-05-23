8988e5205a067935d170b26e9eea1447155ada27
Carlos-Ojeda <a01741085@tec.mx>
2022-05-22 23:47:42 -0600

    practice6.sql

---
+++ practice6.sql
@@ -0,0 +1,26 @@
+-- Index:
+  -- Indexes represent the data with another data structure (trees or hashes) to efficiently perform a given query.
+    CREATE TABLE new_york_addresses (
+        longitude numeric(9,6),
+        latitude numeric(9,6),
+        street_number text,
+        street text,
+        unit text,
+        postcode text,
+        id integer CONSTRAINT new_york_key PRIMARY KEY
+    );
+
+    COPY new_york_addresses
+    FROM '/temp/city_of_new_york.csv'
+    WITH (FORMAT CSV, HEADER);
+
+
+    SELECT * FROM new_york_addresses -- compare the time required
+    WHERE street = 'BROADWAY';
+
+    -- Creating a B-tree index on the street column from new_york_addresses table:
+
+    CREATE INDEX street_idx ON new_york_addresses (street);
+
+    SELECT * FROM new_york_addresses -- compare the time required
+    WHERE street = 'BROADWAY';
